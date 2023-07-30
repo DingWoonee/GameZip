7228144ea94505a2464ed3dd591ccb0c6b2d6657
SungWoon Kim <slm0523@naver.com>
2023-07-30 18:36:36 +0900

    main edit

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,17 @@
 window.addEventListener('load', function() {
+    let gameElemArray = document.querySelector('.container').children;
+    console.log(gameElemArray[0]);
     
+    for(var i = 0; i < gameElemArray.length; i++) {
+        
+    }
 });
+
+function goGame(mainElem, gameElem) {
+    mainElem.style.display = 'none';
+    gameElem.style.display = 'block';
+}
+function goMain(gameElem, mainElem) {
+    gameElem.style.display = 'none';
+    mainElem.style.display = 'grid';
+}
